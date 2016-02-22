bd0f3e0682b82a48e8608557c971ba9e25668fa3
Raafat <alhoumaidy93@gmail.com>
2016-02-22 13:08:35 +0200

    SOLVE MATCHES UPDATE AND NOTIFICATOINS

--- app/src/main/java/com/smartgateapps/spanifootball/activities/MainActivity.java
+++ app/src/main/java/com/smartgateapps/spanifootball/activities/MainActivity.java
@@ -13,7 +13,6 @@ import android.preference.PreferenceFragment;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
